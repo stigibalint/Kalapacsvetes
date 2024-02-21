0207ff8179d75088d920ed35a6d17d254af65883
stigibalint <72031994+stigibalint@users.noreply.github.com>
2024-02-21 10:49:45 +0100

    Add files via upload

---
+++ Kalapacsvetes/Program.cs
@@ -0,0 +1,45 @@
+﻿using Kalapacsvetes;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+List<Versenyző> versenyzoAdatok = new List<Versenyző>();
+
+try
+{
+    File.ReadAllLines("Selejtezo2012.txt").Skip(1).ToList().ForEach(adatok => versenyzoAdatok.Add(new Versenyző(adatok)));
+}
+catch (IOException e)
+{
+    Console.WriteLine($"Hiba történt a fájl olvasása közben: {e.Message}");
+}
+Console.WriteLine($"5. feladat: Versenyzők száma a selejtezőben: {versenyzoAdatok.Count} fő");
+int tovabbjutok = versenyzoAdatok.Count(v => v.D1 > 78 || v.D2 > 78);
+Console.WriteLine($"6. feladat: 78,00 méter feletti eredménnyel továbbjutott: {tovabbjutok}");
+
+Versenyző legjobbEredményVersenyző = versenyzoAdatok.OrderByDescending(v => v.LegnagyobbDobas()).First();
+
+
+Console.WriteLine($"9. feladat: A selejtező nyertese:");
+Console.WriteLine($"\tNév: {legjobbEredményVersenyző.Nev}");
+Console.WriteLine($"\tCsoport: {legjobbEredményVersenyző.Csoport}");
+Console.WriteLine($"\tNemzet és kód: {legjobbEredményVersenyző.NemzetEsKod}");
+Console.WriteLine($"\tLegjobb eredmény: {legjobbEredményVersenyző.LegnagyobbDobas()}");
+
+
+
+using (StreamWriter file = new StreamWriter("Dontos2012.txt", false, Encoding.UTF8))
+{
+    file.WriteLine("Név;Csoport;Nemzet és kód;D1;D2;D3");
+    int szamol = 1;
+    for (int i = 0; i < 12 && i < versenyzoAdatok.Count; i++)
+    {
+        Versenyző elsoTizenketto = versenyzoAdatok.OrderByDescending(v => v.LegnagyobbDobas()).ToList()[i];
+        file.WriteLine($"{szamol};{elsoTizenketto.Nev};{elsoTizenketto.Csoport};{elsoTizenketto.NemzetEsKod};{elsoTizenketto.D1};{elsoTizenketto.D2};{elsoTizenketto.D3}");
+        szamol++;
+    }
+}
+
+Console.WriteLine("A Dontos2012.txt fájl létrehozva az első 12 versenyző adataival.");
+
+Console.ReadLine(); 
---
+++ Kalapacsvetes/Versenyző.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    public class Versenyző
+    {
+        string nev;
+        string csoport;
+        string nemzetEsKod;
+        double d1, d2, d3;
+
+        public Versenyző(string nev, string csoport, string nemzetEsKod, double d1, double d2, double d3)
+        {
+            this.Nev = nev;
+            this.Csoport = csoport;
+            this.NemzetEsKod = nemzetEsKod;
+            this.D1 = d1;
+            this.D2 = d2;
+            this.D3 = d3;
+        }
+        public Versenyző(string sor)
+        {
+            var adatok = sor.Split(';');
+            Nev = adatok[0];
+            Csoport =adatok[1];
+            NemzetEsKod = adatok[2];
+            D1 = ParseDobas(adatok[3]);
+            D2 = ParseDobas(adatok[4]);
+            D3 = ParseDobas(adatok[5]);
+        }
+        public double ParseDobas(string eredmeny)
+        {
+            switch (eredmeny)
+            {
+                case "X":
+                    return -1.0;
+                case "-":
+                    return -2.0; 
+                default:
+                    return Convert.ToDouble(eredmeny);
+            }
+        }
+
+
+        public double LegnagyobbDobas()
+        {
+            double[] dobások = { D1, D2, D3 };
+            return dobások.Max();
+        }
+
+        public double Eredmény()
+        {
+            return LegnagyobbDobas();
+        }
+
+        public string Nemzet()
+        {
+            return NemzetEsKod.Split(' ')[0];
+        }
+
+        public string Kód()
+        {
+            return NemzetEsKod.Split(' ')[1].Trim('(', ')');
+        }
+
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Csoport { get => csoport; set => csoport = value; }
+        public string NemzetEsKod { get => nemzetEsKod; set => nemzetEsKod = value; }
+        public double D1 { get => d1; set => d1 = value; }
+        public double D2 { get => d2; set => d2 = value; }
+        public double D3 { get => d3; set => d3 = value; }
+    }
+}
